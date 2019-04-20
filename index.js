61b733eaaaca8a57d2f38ce8e85f607f61ed5c46
Akshay.Damodaran <akshay.damodaran@ril.com>
2019-04-21 02:46:56 +0530

    Added api endpoint for frequenct count

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 
 const conf = require('./conf.json');
+const getCount = require('./models/getCount');
 
 const app = express();
 
 app.use(morgan('dev'));
+
+app.get('/frequency', async (req, res) => {
+  const { n } = req.query;
+  try {
+    const result = await getCount(n);
+    res.send({ result });
+  } catch (err) {
+    res.status(500).send({ err });
+  }
+});
 
 app.listen(conf.port, () => console.log(`Server started on port ${conf.port}!`));
