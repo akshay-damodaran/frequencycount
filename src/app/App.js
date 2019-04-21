d658138b2aae34e453915303c757c282f2d1c64e
Akshay.Damodaran <akshay.damodaran@ril.com>
2019-04-21 13:55:42 +0530

    Rendering data recieved from api

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import getApi from '../request';
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       n: 0,
+      data: [],
     };
   }
 
@@ -14,13 +17,54 @@ class App extends React.Component {
     });
   }
 
-  render() {
+  async handleClick() {
+    const url = "http://localhost:3000/frequency";
     const { n } = this.state;
+    const { isError, response } = await getApi(url, { n });
+    if (isError) {
+      console.log('Couldn\'t load data');
+      this.setState({
+        data: [],
+      })
+    } else {
+      this.setState({
+        data: response.data.result,
+      })
+    }
+  }
+
+  renderTable(data) {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Sr no.</th>
+            <th>Word</th>
+            <th>Frequency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, i) => (
+            <tr key={row.word}>
+              <td>{i + 1}</td>
+              <td>{row.word}</td>
+              <td>{row.count}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )
+  }
+
+  render() {
+    const { n, data } = this.state;
     return (
       <div>
         <p>Frequency Count</p>
         <label htmlFor="n-input">Enter value of n : </label>
         <input type="number" value={n} onChange={e => this.handleChange(Number(e.target.value))} />
+        <button type="button" onClick={() => this.handleClick()}>Submit</button>
+        {data.length > 0 && this.renderTable(data)}
       </div>
     );
   }
